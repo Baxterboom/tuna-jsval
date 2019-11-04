ddae053a6d3b60745f69cddb361038d9daae14c3
Baxterboom <baxterboom.1@gmail.com>
2019-11-04 23:50:03 +0100

    npm audit fix

--- gulpfile.js
+++ gulpfile.js
@@ -8,24 +8,14 @@ var path = {
 
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var sourcemaps = require('gulp-sourcemaps');
 
-//tasks - sass
-gulp.task('scss:compile', function () {
-    gulp.src(path.css + '**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.dist));
-    //.pipe(gulp.dest(path.projdir));
-});
-
 //tasks - typescripts
-gulp.task('typescript:compile', function () {
-    var tsSrc = ts.createProject("tsconfig.json", { outFile: name + '.js' });
-    var tsTests = ts.createProject("tsconfig.tests.json", { outFile: name + '-tests.js' });
+gulp.task('ts:compile', function () {
+    var tsSrc = ts.createProject("tsconfig.json", {
+        outFile: name + '.js'
+    });
 
     var minifyOptions = {
         ext: {
@@ -36,28 +26,31 @@ gulp.task('typescript:compile', function () {
         ignoreFiles: ['.combo.js', '-min.js']
     };
 
-    return [
-        gulp.src(path.js + '**/*.ts')
-            //.pipe(sourcemaps.init())
-            .pipe(tsSrc())
-            .pipe(minify(minifyOptions))
-            //.pipe(sourcemaps.write())
-            .pipe(gulp.dest(path.dist)),
+    return gulp.src(path.js + '**/*.ts')
+        //.pipe(sourcemaps.init())
+        .pipe(tsSrc())
+        .pipe(minify(minifyOptions))
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist));
+});
 
-        gulp.src(path.js + '**/*-test.ts')
-            //.pipe(sourcemaps.init())
-            .pipe(tsTests())
-            //.pipe(minify(minifyOptions))
-            //.pipe(sourcemaps.write())
-            .pipe(gulp.dest(path.dist))
-    ];
+
+gulp.task('ts-test:compile', function () {
+    var tsTests = ts.createProject("tsconfig.tests.json", {
+        outFile: name + '-tests.js'
+    });
+
+    return gulp.src(path.js + '**/*-test.ts')
+        //.pipe(sourcemaps.init())
+        .pipe(tsTests())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist));
 });
 
 //watchers
 gulp.task('file:watch', function () {
-    gulp.watch(path.js + '**/*.ts', ['typescript:compile']);
-    gulp.watch(path.css + '**/*.scss', ['scss:compile']);
+    gulp.watch(path.js + '**/*.ts', gulp.series(['ts:compile']));
 });
 
 //setup build
-gulp.task('build', ['scss:compile', 'typescript:compile']);
+gulp.task('build', gulp.series(['ts:compile', 'ts-test:compile']));
